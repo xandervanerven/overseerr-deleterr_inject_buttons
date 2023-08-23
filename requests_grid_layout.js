c1150fb93f5542d2072fe14c593f0cc10edd24a1
Xander <xander@nfc-nederland.nl>
2023-08-24 00:11:48 +0200

    character fix

--- requests_grid_layout.js
+++ requests_grid_layout.js
@@ -1,22 +1,19 @@
 // De CSS die je wilt toepassen
-const customCSS = `
-    .max-w-8xl.mx-auto.px-4 {
-        display: grid;
-        grid-template-columns: repeat(auto-fill,minmax(33rem,1fr));
-        gap: 1rem;
-        align-items: center;
-    }
+const customCSS = 
+    ".max-w-8xl.mx-auto.px-4 {" +
+        "display: grid;" +
+        "grid-template-columns: repeat(auto-fill,minmax(33rem,1fr));" +
+        "gap: 1rem;" +
+        "align-items: center;" +
+    "}" +
     
-    .relative.z-10.flex.w-full.items-center.overflow-hidden.pl-4.pr-4.sm\:pr-0.xl\:w-7\/12` + 
-    '.32' +
-    `xl\:w-2\/3 {
-        width: 50%;
-    }
+    ".relative.z-10.flex.w-full.items-center.overflow-hidden.pl-4.pr-4.sm\\:pr-0.xl\\:w-7\\/12.\\32xl\\:w-2\\/3 {" +
+        "width: 50%;" +
+    "}" +
     
-    .z-10.mt-4.flex.w-full.flex-col.justify-center.space-y-2.pl-4.pr-4.xl\:mt-0.xl\:w-96.xl\:items-end.xl\:pl-0 {
-        display: none;
-    }
-`;
+    ".z-10.mt-4.flex.w-full.flex-col.justify-center.space-y-2.pl-4.pr-4.xl\\:mt-0.xl\\:w-96.xl\\:items-end.xl\\:pl-0 {" +
+        "display: none;" +
+    "}";
 
 let styleElement; // Dit element zal de custom CSS bevatten
 

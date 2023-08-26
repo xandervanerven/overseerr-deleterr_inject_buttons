b3122ff2a0d14837aa6300de5dcda0e8bb64a826
Xander <xander@nfc-nederland.nl>
2023-08-26 21:25:00 +0200

    classes

--- gotify_poll.js
+++ gotify_poll.js
@@ -9,9 +9,17 @@ function showPopupMessage(message) {
         alertifyDialog = alertify.alert("Overseerr deleterr", messagesContent).set({transition:'zoom'}).show();
         
         // Pas hier de stijl van de dialog toe
-        let dialogElem = document.querySelector('.alertify .ajs-content');
+        let dialogElem = document.querySelector('.alertify .ajs-dialog');
+        let dialogHeader = document.querySelector('.alertify .ajs-header');
+        let dialogContent = document.querySelector('.alertify .ajs-content');
         if (dialogElem) {
             dialogElem.classList.add('shadow-xl', 'sm:rounded-lg', 'bg-gray-800', 'text-gray-200');
+        }
+        if (dialogHeader) {
+            dialogElem.classList.add('text-white');
+        }
+        if (dialogContent) {
+            dialogElem.classList.add('shadow-xl', 'bg-gray-900', 'text-gray-200');
         }
         
         alertifyDialog.setting('onclose', function() { 

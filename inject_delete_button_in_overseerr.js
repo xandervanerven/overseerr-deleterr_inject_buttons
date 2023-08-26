603c25befeb7ddea4253347dc2b3ac37babae2d2
Xander <xander@nfc-nederland.nl>
2023-08-26 19:57:50 +0200

    stop

--- inject_delete_button_in_overseerr.js
+++ inject_delete_button_in_overseerr.js
@@ -1,8 +1,8 @@
 const setupCustomButtons = () => {
     const currentURL = window.location.href;
-    if (!currentURL.includes('/movie/') && !currentURL.includes('/tv/')) {
-        return; // Stop de uitvoering als de URL niet de vereiste strings bevat
-    }
+    if (!document.querySelector('button.bg-yellow-500') || (!currentURL.includes('/movie/') && !currentURL.includes('/tv/'))) {
+        stopGotifyPolling();
+    }    
 
     const geleButton = document.querySelector('button.bg-yellow-500');
 

2db3990ecbd23306944a9f559c09f61a668dc3ca
Xander <xander@nfc-nederland.nl>
2023-08-26 22:35:14 +0200

    fix

--- gotify_poll.js
+++ gotify_poll.js
@@ -1,46 +1,52 @@
 let gotifyInterval;
-let alertifyDialog = null;  
-let messagesContent = ""; 
+let alertifyDialog = null;
+let messagesContent = "";
 
 const currentURL = window.location.pathname;
 
 function showPopupMessage(message) {
-    messagesContent += message + "<br>";  
+    messagesContent += message + "<br>";
 
     if (!alertifyDialog) {
-        alertifyDialog = alertify.alert("Overseerr deleterr", messagesContent).set({transition:'zoom'}).show();
-        
-        // Pas hier de stijl van de dialog toe
-        let dialogElem = document.querySelector('.alertify .ajs-dialog');
-        let dialogHeader = document.querySelector('.alertify .ajs-header');
-        let dialogContent = document.querySelector('.alertify .ajs-content');
-        let dialogFooter = document.querySelector('.alertify .ajs-footer');
-        let dialogDimmer = document.querySelector('.alertify .ajs-dimmer');
+        alertifyDialog = alertify.alert("Overseerr deleterr", messagesContent).set({
+            transition: 'zoom'
+        }).show();
 
-        if (dialogElem) {
-            dialogElem.classList.add('ring-1','shadow-xl', 'sm:rounded-lg', 'bg-gray-800', 'text-gray-200');
-            dialogElem.style.cssText += 'border: 0px !important; border-radius: 0.5rem !important; background-color: rgb(31 41 55/var(--tw-bg-opacity)) !important;';
-        }        
-        if (dialogHeader) {
-            dialogHeader.classList.add('text-white');
-            dialogHeader.style.cssText += 'background-color: rgb(31 41 55/var(--tw-bg-opacity)) !important; color: rgb(255 255 255/var(--tw-text-opacity)) !important;';
-        }
-        if (dialogContent) {
-            dialogContent.classList.add('text-gray-200', 'sm:rounded-lg');
-            dialogContent.style.cssText += 'background-color: rgb(55 65 81/var(--tw-bg-opacity)) !important;';
-        }
-        if (dialogFooter) {
-            dialogFooter.style.cssText += 'background-color: rgb(31 41 55/var(--tw-bg-opacity)) !important;';
-        }        
-        if (dialogDimmer) {
-            dialogDimmer.style.cssText += 'background-color: rgba(0,0,0,.65) !important;';
-        }        
-        
-        alertifyDialog.setting('onclose', function() { 
-            alertifyDialog = null;  
+        // Pas hier de stijl van de dialog toe
+        styleAlertifyDialog();
+
+        alertifyDialog.setting('onclose', function() {
+            alertifyDialog = null;
         });
     } else {
         alertifyDialog.setContent(messagesContent);
+    }
+}
+
+function styleAlertifyDialog() {
+    let dialogElem = document.querySelector('.alertify .ajs-dialog');
+    let dialogHeader = document.querySelector('.alertify .ajs-header');
+    let dialogContent = document.querySelector('.alertify .ajs-content');
+    let dialogFooter = document.querySelector('.alertify .ajs-footer');
+    let dialogDimmer = document.querySelector('.alertify .ajs-dimmer');
+
+    if (dialogElem) {
+        dialogElem.classList.add('ring-1', 'shadow-xl', 'sm:rounded-lg', 'bg-gray-800', 'text-gray-200');
+        dialogElem.style.cssText += 'border: 0px !important; border-radius: 0.5rem !important; background-color: rgb(31 41 55/var(--tw-bg-opacity)) !important;';
+    }
+    if (dialogHeader) {
+        dialogHeader.classList.add('text-white');
+        dialogHeader.style.cssText += 'background-color: rgb(31 41 55/var(--tw-bg-opacity)) !important; color: rgb(255 255 255/var(--tw-text-opacity)) !important;';
+    }
+    if (dialogContent) {
+        dialogContent.classList.add('text-gray-200', 'sm:rounded-lg');
+        dialogContent.style.cssText += 'background-color: rgb(55 65 81/var(--tw-bg-opacity)) !important;';
+    }
+    if (dialogFooter) {
+        dialogFooter.style.cssText += 'background-color: rgb(31 41 55/var(--tw-bg-opacity)) !important;';
+    }
+    if (dialogDimmer) {
+        dialogDimmer.style.cssText += 'background-color: rgba(0,0,0,.65) !important;';
     }
 }
 
@@ -50,8 +56,9 @@ function stopGotifyPolling() {
 
 function startGotifyPolling() {
     stopGotifyPolling();
-    messagesContent = "";  
+    messagesContent = "";
 
+    const initialURL = window.location.pathname;
     const seenMessageIds = new Set();
     let isFirstRun = true;
 
@@ -66,36 +73,40 @@ function startGotifyPolling() {
                 return response.json();
             })
             .then(data => {
-                if (data && data.messages && Array.isArray(data.messages) && data.messages.length > 0) {
-                    data.messages.reverse().forEach(message => {
-                        const messageText = message.message;
-
-                        if (isFirstRun && (messageText.includes("Verzoek om") || messageText.includes("Request to"))) {
-                            isFirstRun = false;
-                        }
-
-                        if (!isFirstRun && !seenMessageIds.has(message.id)) {
-                            console.log("Gotify Message:", message.message);
-                            showPopupMessage(message.message);
-                            seenMessageIds.add(message.id);
-
-                            if (seenMessageIds.size > 100) {
-                                seenMessageIds.clear();
-                            }
-                        } else if (isFirstRun) {
-                            seenMessageIds.add(message.id);
-                        }
-                    });
-                }
+                processGotifyData(data);
             })
             .catch(error => {
                 console.log("There was a problem with the fetch operation:", error.message);
             })
             .finally(() => {
-                if (!document.querySelector('button.bg-yellow-500') || (!currentURL.includes('/movie/') && !currentURL.includes('/tv/'))) {
+                if (window.location.pathname !== initialURL || !document.querySelector('button.bg-yellow-500') || (!currentURL.includes('/movie/') && !currentURL.includes('/tv/'))) {
                     stopGotifyPolling();
-                }                
+                }
             });
+    }
+
+    function processGotifyData(data) {
+        if (data && data.messages && Array.isArray(data.messages) && data.messages.length > 0) {
+            data.messages.reverse().forEach(message => {
+                const messageText = message.message;
+
+                if (isFirstRun && (messageText.includes("Verzoek om") || messageText.includes("Request to"))) {
+                    isFirstRun = false;
+                }
+
+                if (!isFirstRun && !seenMessageIds.has(message.id)) {
+                    console.log("Gotify Message:", message.message);
+                    showPopupMessage(message.message);
+                    seenMessageIds.add(message.id);
+
+                    if (seenMessageIds.size > 100) {
+                        seenMessageIds.clear();
+                    }
+                } else if (isFirstRun) {
+                    seenMessageIds.add(message.id);
+                }
+            });
+        }
     }
 
     gotifyInterval = setInterval(fetchGotifyMessages, 1000);
@@ -103,3 +114,8 @@ function startGotifyPolling() {
     // Stop het pollen na 30 seconden
     setTimeout(stopGotifyPolling, 30000);
 }
+
+// Event listener om het pollen te stoppen wanneer de URL van de pagina verandert
+window.addEventListener('popstate', function(event) {
+    stopGotifyPolling();
+});
